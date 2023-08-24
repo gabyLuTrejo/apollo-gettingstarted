260fc60204a2fa70a8e667773195127704f6bfc9
gabyLuTrejo <gabytrejo303@gmail.com>
2023-08-24 17:45:07 +0000

    mock Data sources

---
+++ src/index-mockDataSource.ts
@@ -0,0 +1,67 @@
+import { ApolloServer } from "@apollo/server"
+import { startStandaloneServer } from '@apollo/server/standalone'
+
+
+//Define schema
+const typeDefs = `
+  type Book {
+   title:String
+   author:String
+  }
+
+  type Query {
+     books:[Book]
+  }
+
+`
+const BOOKS = [{
+    title: 'a',
+    author: 'AuthorA'
+},
+{
+    title: 'Typescript in Action',
+    author: 'b'
+}]
+
+//// Data Source Class
+export class BookDataSource {
+    //api
+    getBooks() {
+        return BOOKS
+    }
+}
+const resolvers = {
+    //Query 
+    Query: {
+        books(parent,args,ctx) {
+            return ctx.dataSources.booksAPI.getBooks()
+        }
+    }
+
+}
+
+//context Type
+type MyContext = {
+    dataSources: {
+        booksAPI: BookDataSource
+    }
+}
+
+const server = new ApolloServer({
+    typeDefs: typeDefs,
+    resolvers: resolvers
+})
+//Start the Webserver and deploy
+const { url } = await startStandaloneServer(server, {
+    listen: {
+        port: 4000
+    }, 
+    context: async () => {
+        return {
+            dataSources: {
+                booksAPI: new BookDataSource()
+            }
+        }
+    }
+})
+console.log(`Apollo Server is Started at ${url}`)
